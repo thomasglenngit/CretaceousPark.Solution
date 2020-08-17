b1e9b967ef33072ccb2850c58b362cd9dd8ea1c3
Thomas Glenn <thomasgrantglenn@mac.com>
2020-08-17 08:42:41 -0600

    add .vscode to gitignore.

--- CretaceousPark/Controllers/AnimalsController.cs
+++ CretaceousPark/Controllers/AnimalsController.cs
@@ -17,8 +17,32 @@ namespace CretaceousPark.Controllers
       _db = db;
     }
 
-    //GET api/animals
+    // GET api/animals
     [HttpGet]
+    public ActionResult<IEnumerable<Animal>> Get(string species, string gender, string name)
+    {
+      var query = _db.Animals.AsQueryable();
+
+      if (species != null)
+      {
+        query = query.Where(entry => entry.Species == species);
+      }
+
+      if (gender != null)
+      {
+        query = query.Where(entry => entry.Gender == gender);
+      }
+
+      if (name != null)
+      {
+        query = query.Where(entry => entry.Name == name);
+      }
+
+      return query.ToList();
+    }
+
+    // POST api/animals
+    [HttpPost]
     public void Post([FromBody] Animal animal)
     {
       _db.Animals.Add(animal);
@@ -29,16 +53,16 @@ namespace CretaceousPark.Controllers
     [HttpGet("{id}")]
     public ActionResult<Animal> Get(int id)
     {
-        return _db.Animals.FirstOrDefault(entry => entry.AnimalId == id);
+      return _db.Animals.FirstOrDefault(entry => entry.AnimalId == id);
     }
 
     // PUT api/animals/5
-    [HttpPut("{id}")]//Annotation? I thought this was called an attribute?
+    [HttpPut("{id}")]
     public void Put(int id, [FromBody] Animal animal)
     {
-        animal.AnimalId = id;
-        _db.Entry(animal).State = EntityState.Modified;
-        _db.SaveChanges();
+      animal.AnimalId = id;
+      _db.Entry(animal).State = EntityState.Modified;
+      _db.SaveChanges();
     }
 
     // DELETE api/animals/5
@@ -48,18 +72,6 @@ namespace CretaceousPark.Controllers
       var animalToDelete = _db.Animals.FirstOrDefault(entry => entry.AnimalId == id);
       _db.Animals.Remove(animalToDelete);
       _db.SaveChanges();
-    }
-
-    // GET api/animals
-    [HttpGet]
-    public ActionResult<IEnumerable<Animal>> Get(string species)
-    {
-      var query = _db.Animals.AsQueryable();
-      if (species != null)
-      {
-        query = query.Where(entry => entry.Species == species);
-      }
-      return query.ToList();
     }
   }
 }
